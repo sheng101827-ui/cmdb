c0cb53c1815c0315804f06cc5bf2cfd1717fd45c
xxx <sheneg101827@gmail>
2026-05-25 11:49:48 +0000

    1.1

--- package_upload.py
+++ package_upload.py
@@ -24,6 +24,7 @@ def get_current_branch():
 if __name__ == '__main__':
     dt = datetime.datetime.now()
     msg = dt.strftime("%Y%m%d%H%M")
+    msg = raw_input("请输入commit描述: ")
     current_branch = get_current_branch()
 
     run_cmd("git add *")
